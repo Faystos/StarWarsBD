2d2706f48bf0b81f686ca10f87f32fa80610aaaf
Vladimir <fayst_@bk.ru>
2020-09-14 04:00:50 +0300

    Добавил компонент прилоадера и поработал над его логикой.

---
+++ src/componets/loader/Loader.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './loader.css';
+
+ const Loader = () => {
+  return (
+  <div className="loadingio-spinner-double-ring-b3okl9wpgv mx-auto">
+    <div className="ldio-oabdd8yrpxn">
+      <div></div>
+      <div></div>
+      <div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default Loader;
--- src/componets/randomPlanet/RandomPlanet.js
+++ src/componets/randomPlanet/RandomPlanet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SwapiService from '../../services/Services';
+import Loader from '../loader';
 import './randomPlanet.css';
 
 export default class RandomPlanet extends Component {
@@ -7,6 +8,7 @@ export default class RandomPlanet extends Component {
 
   state = {
     planet: {},
+    load: true,
   };
 
   constructor() {
@@ -15,7 +17,7 @@ export default class RandomPlanet extends Component {
   }
 
   onPlanetLoaded = planet => {
-    this.setState({ planet });
+    this.setState({ planet, load: false });
   }
 
   updatePlanet() {
@@ -24,29 +26,40 @@ export default class RandomPlanet extends Component {
   }
 
   render() {
-  const { planet: { id, planetName, population, rotationPeriod, diameter } } = this.state;
-
+  const { planet, load } = this.state;
+  const loader = load ? <Loader/> : null;
+  const viewPlanet = load ? null : <PlanetView planet = { planet }/>
     return (
-      <div className="random-planet jumbotron rounded">
-        <img className="planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt={`Планета ${planetName}`}/>
-        <div>
-          <h4>{ planetName }</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{ population }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{ rotationPeriod }</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{ diameter }</span>
-            </li>
-          </ul>
-        </div>
+      <div className = "random-planet jumbotron rounded">      
+        { loader } 
+        { viewPlanet }
       </div>
     );
   }
+}
+
+const PlanetView = ({ planet }) => {  
+  const { id, planetName, population, rotationPeriod, diameter } = planet;
+  return (    
+    <React.Fragment>
+      <img className="planet-image" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt={`Планета ${planetName}`}/>
+      <div>
+        <h4>{ planetName }</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population</span>
+            <span>{ population }</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period</span>
+            <span>{ rotationPeriod }</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter</span>
+            <span>{ diameter }</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
 }

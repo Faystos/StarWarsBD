5401d244f1e0212d416d384fa83cccf99b80c3a2
Vladimir <fayst_@bk.ru>
2020-12-24 19:20:42 +0300

    Updates

---
+++ precache-manifest.4979762cf3f7691105168f1ce8b22d02.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9048c10de8b48fbfe744ef6d790b91",
+    "url": "/StarWarsDB/index.html"
+  },
+  {
+    "revision": "682dddd6b7045e793aa8",
+    "url": "/StarWarsDB/static/css/main.44ae125c.chunk.css"
+  },
+  {
+    "revision": "21978fecbd53c97882e0",
+    "url": "/StarWarsDB/static/js/2.1c4dd06c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StarWarsDB/static/js/2.1c4dd06c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "682dddd6b7045e793aa8",
+    "url": "/StarWarsDB/static/js/main.52ca04f1.chunk.js"
+  },
+  {
+    "revision": "41f0e23869ef55e48131",
+    "url": "/StarWarsDB/static/js/runtime-main.3ab0e05c.js"
+  },
+  {
+    "revision": "273b84b0f1ae3e71923e9bd1026428bc",
+    "url": "/StarWarsDB/static/media/people.273b84b0.png"
+  },
+  {
+    "revision": "ee27eebe2ade28367b61c30f45cb09a6",
+    "url": "/StarWarsDB/static/media/planet.ee27eebe.png"
+  },
+  {
+    "revision": "1c502e0d4d3a1f3fecb60608f2c48155",
+    "url": "/StarWarsDB/static/media/sprite.1c502e0d.svg"
+  },
+  {
+    "revision": "22342cf805c9f7b376e7378060eab8d6",
+    "url": "/StarWarsDB/static/media/starShips.22342cf8.png"
+  }
+]);
